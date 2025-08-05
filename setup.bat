ef4bebdada26515b9a9bdf09ae073a0b86333583
Sachanime <escalierquitue@gmail.com>
2025-08-05 16:16:56 +0200

    Update setup.bat
    Setup.bat couldn't download required files

--- setup.bat
+++ setup.bat
@@ -1,4 +1,5 @@
 sudo config --enable normal
+mkdir "C:\Program Files\SKL\Auto-Winget"
 curl -L -o "C:\Program Files\SKL\Auto-Winget\Auto-Winget.ps1" https://raw.githubusercontent.com/Sachanime/Auto-winget/main/Auto-Winget.ps1
 curl -L -o "C:\Program Files\SKL\Auto-Winget\Auto-winget.xml" https://raw.githubusercontent.com/Sachanime/Auto-winget/main/Auto-winget.xml
 powershell Register-ScheduledTask -Xml (Get-Content "C:\Program Files\SKL\Auto-Winget\Auto-winget.xml" | Out-String) -TaskName "Auto-Winget"
