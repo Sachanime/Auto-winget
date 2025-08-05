b7b679118cc7ce96e6000298221a04c3082d9544
Sachanime <escalierquitue@gmail.com>
2025-08-05 16:57:22 +0200

    ExecutionPolicy Bypass

--- setup.bat
+++ setup.bat
@@ -2,5 +2,5 @@ sudo config --enable normal
 mkdir "C:\Program Files\SKL\Auto-Winget"
 curl -L -o "C:\Program Files\SKL\Auto-Winget\Auto-Winget.ps1" https://raw.githubusercontent.com/Sachanime/Auto-winget/main/Auto-Winget.ps1
 curl -L -o "C:\Program Files\SKL\Auto-Winget\Auto-winget.xml" https://raw.githubusercontent.com/Sachanime/Auto-winget/main/Auto-winget.xml
-powershell Register-ScheduledTask -Xml (Get-Content "C:\Program Files\SKL\Auto-Winget\Auto-winget.xml" | Out-String) -TaskName "Auto-Winget"
+powershell -Command "Register-ScheduledTask -Xml (Get-Content 'C:\Program Files\SKL\Auto-Winget\Auto-winget.xml' -Raw) -TaskName 'Auto-Winget'"
 pause
