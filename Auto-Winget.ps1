459d6a64d72db525e50e03a63d2d5b0861690af6
Sachanime <escalierquitue@gmail.com>
2025-08-05 12:56:08 +0200

    New features
    Script running in background
    Script now located in `C:\Program Files\SKL\Auto-Winget`

--- Auto-Winget.ps1
+++ Auto-Winget.ps1
@@ -1,3 +1,38 @@
-﻿taskkill /F /IM EpicGamesLauncher.exe
+﻿Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class WinAPI {
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetConsoleWindow();
+
+    [DllImport("user32.dll")]
+    public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter,
+        int X, int Y, int cx, int cy, uint uFlags);
+
+    public static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+    public const UInt32 SWP_NOMOVE = 0x0002;
+    public const UInt32 SWP_NOSIZE = 0x0001;
+    public const UInt32 SWP_NOACTIVATE = 0x0010;
+
+}
+"@
+
+$hwnd = [WinAPI]::GetConsoleWindow()
+
+if ($hwnd -ne [IntPtr]::Zero) {
+
+    [WinAPI]::SetWindowPos($hwnd, [WinAPI]::HWND_BOTTOM, 0, 0, 0, 0,
+    [WinAPI]::SWP_NOMOVE -bor [WinAPI]::SWP_NOSIZE -bor [WinAPI]::SWP_NOACTIVATE)
+
+} 
+
+else {
+
+    Write-Host "Impossible de récupérer le handle de la console."
+    
+}
+
+taskkill /F /IM EpicGamesLauncher.exe
 winget upgrade --all --accept-package-agreements --accept-source-agreements
 winget upgrade --wait
