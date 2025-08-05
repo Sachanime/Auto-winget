ebda0ba74538eaeae4052761be863a7d95d09d41
Sachanime <escalierquitue@gmail.com>
2025-08-05 16:18:23 +0200

    Update setup.bat
    Add pause for check script result

--- setup.bat
+++ setup.bat
@@ -3,3 +3,4 @@ mkdir "C:\Program Files\SKL\Auto-Winget"
 curl -L -o "C:\Program Files\SKL\Auto-Winget\Auto-Winget.ps1" https://raw.githubusercontent.com/Sachanime/Auto-winget/main/Auto-Winget.ps1
 curl -L -o "C:\Program Files\SKL\Auto-Winget\Auto-winget.xml" https://raw.githubusercontent.com/Sachanime/Auto-winget/main/Auto-winget.xml
 powershell Register-ScheduledTask -Xml (Get-Content "C:\Program Files\SKL\Auto-Winget\Auto-winget.xml" | Out-String) -TaskName "Auto-Winget"
+pause
