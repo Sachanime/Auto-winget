c97c2d2349aa05875f03067b325b53520e6207b0
Sachanime <escalierquitue@gmail.com>
2026-01-17 15:38:27 +0100

    Update Auto-Winget.ps1
    Patch for Roblox updates

--- Auto-Winget.ps1
+++ Auto-Winget.ps1
@@ -29,10 +29,30 @@ if ($hwnd -ne [IntPtr]::Zero) {
 
 else {
 
-    Write-Host "Impossible de récupérer le handle de la console."
+    Write-Host "Impossible de récupérer le handle de la console." -ForegroundColor Red
     
 }
 
 taskkill /F /IM EpicGamesLauncher.exe
-sudo pwsh -WindowStyle hidden -Command {winget upgrade --all --accept-package-agreements --accept-source-agreements}
-winget upgrade --wait
+sudo pwsh -WindowStyle hidden -Command {winget upgrade --all --include-unknown --accept-package-agreements --accept-source-agreements --silent}
+
+# Roblox Patch
+if(winget upgrade --id Roblox.Roblox --include-unknown 2>&1 -match "Version") {
+
+    $RobloxRegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\roblox-player"
+    $RobloxRegObject = Get-ItemProperty -Path $RobloxRegPath
+    $RobloxPlayer = $RobloxRegObject.InstallLocation + "\RobloxPlayerBeta.exe"
+    $RobloxVersion = (Get-Item $RobloxPlayer).VersionInfo.FileVersion -replace ', ', '.'
+
+    Write-Host "Roblox installation detected, patching Windows Registry..." -ForegroundColor Blue
+    Start-Sleep 1
+
+    $RobloxRegPath = "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\roblox-player"
+    REG ADD $RobloxRegPath /v DisplayVersion /t REG_SZ /d $RobloxVersion /f
+
+    Write-Host "Windows Registry patched for Roblox" -ForegroundColor Green
+    Start-Sleep 3
+
+}
+
+winget upgrade --include-unknown --wait
